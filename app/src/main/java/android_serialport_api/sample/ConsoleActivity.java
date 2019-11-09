3a63f9b45abdb94ef630339968baf7d3d3eb37a6
DemoonLucck <demoonlucck@.github.com>
2019-11-09 16:13:34 +0800

    add：字节流转换为16进制字符串      16进制字符串显示为10进制字符串

--- app/src/main/java/android_serialport_api/sample/ConsoleActivity.java
+++ app/src/main/java/android_serialport_api/sample/ConsoleActivity.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,13 +35,16 @@ public class ConsoleActivity extends SerialPortActivity {
 					return;
 				}
 				char[] text = new char[t.length()];
+				StringBuilder sb = new StringBuilder(t.length());
 				for (int i = 0; i < t.length(); i++) {
 					text[i] = t.charAt(i);
+					sb.append(text[i]);
 				}
+				String sendString = sb.toString();
 				try {
 
-					mOutputStream.write(new String(text).getBytes());
-					mOutputStream.write('\n');
+					mOutputStream.write(HexToByteArr(sendString));
+//					mOutputStream.write('\n');
 
 				} catch (IOException e) {
 					e.printStackTrace();
@@ -71,14 +75,60 @@ public class ConsoleActivity extends SerialPortActivity {
 	protected void onDataReceived(final byte[] buffer, final int size) {
 		runOnUiThread(new Runnable() {
 			public void run() {
-				if (mReception != null){
-					mReception.clearComposingText();
-				}
+				//清空接收栏
+				mReception.clearComposingText();
 
-					mReception.append(new String(buffer, 0, size));
-					mReception.append("\n");
+				mReception.setText(String.valueOf(HexToInt(bytesToHexString(buffer,size))));
+				mReception.append("\n");
 
 			}
 		});
 	}
+	//字节数组转Hex字符串
+	public static String bytesToHexString(byte[] bArray,int size) {
+		StringBuilder sb = new StringBuilder(size);
+		String sTemp;
+		for (int i = 0; i < size; i++) {
+			sTemp = Integer.toHexString(0xFF & bArray[i]);
+			if (sTemp.length() < 2) {
+				sb.append(0);
+			}
+			sb.append(sTemp.toUpperCase());
+		}
+		return sb.toString();
+	}
+	//Hex字符串转字节数组
+	public static byte[] HexToByteArr(String inHex) {
+		byte[] result;
+		int hexlen = inHex.length();
+		if (isOdd(hexlen) == 1) {
+			hexlen++;
+			result = new byte[(hexlen / 2)];
+			inHex = "0" + inHex;
+		} else {
+			result = new byte[(hexlen / 2)];
+		}
+		int j = 0;
+		for (int i = 0; i < hexlen; i += 2) {
+			result[j] = HexToByte(inHex.substring(i, i + 2));
+			j++;
+		}
+		return result;
+	}
+	public static int isOdd(int num) {
+		return num & 1;
+	}
+	public static byte HexToByte(String inHex) {
+		return (byte) Integer.parseInt(inHex, 16);
+	}
+
+	//Hex字符串转int
+	public static int HexToInt(String inHex) {
+		return Integer.parseInt(inHex, 16);
+	}
+
+	public static String IntToHex(int intHex){
+		return Integer.toHexString(intHex);
+	}
+
 }
